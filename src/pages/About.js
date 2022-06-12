0d96df7ef8d29a512458c18df327c96372f899b3
bdasgupta027 <bdasgupta027@student.fuhsd.org>
2022-06-12 03:39:13 -0700

    adding files

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profile_pic from "../assets/profile_pic.jpg"; 
+
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+      <div class="split left">
+        <div className="centered">
+          <img 
+            className="profile_image"
+            src={profile_pic}
+            alt="Profile Pic"
+            ></img>
+        </div> 
       </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Bhaswati Das Gupta</div>
+          <div className="brief_description">
+            <p> Hi! My name is Bhaswati Das Gupta. I'm currently a rising sophomore at UC Santa Cruz.
+                Computer Science and minoring in Statistics. Some of my hobbies include playing the
+               flute and dancing. In the future, I hope to go into machine learning or artificial 
+               intelligence.</p> 
+          </div>
+        </div>
+      </div>
+      </div>
+      
     )
   }
 }
+
+//<div>
+
